06ee359903f08eac01853cf50ccb9da49a1e8f7c
kenharsch <kharsch@brandeis.edu>
2015-05-22 16:04:18 -0400

    wiki works, looks good. style changes.

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,8 @@ Rails.application.routes.draw do
   Precious::App.set(:gollum_path, Rails.root.join('wiki').to_s)
   Precious::App.set(:default_markup, :markdown) # set your favorite markup language
   Precious::App.set(:wiki_options, {:universal_toc => true})
-  mount Precious::App, at: 'wiki'
+  Precious::App.set(:wiki_options, {:css => true})
+  mount Precious::App, at: '/wiki'
 
   get 'team/teampage'
 
--- wiki/config.ru
+++ wiki/config.ru
@@ -7,6 +7,7 @@ require 'rubygems'
 require 'gollum/app'
 
 gollum_path = File.expand_path('/wiki', __FILE__) # CHANGE THIS TO POINT TO YOUR OWN WIKI REPO
+wiki_options[:css] = true
 wiki_options = {:universal_toc => false}
 Precious::App.set(:gollum_path, gollum_path)
 Precious::App.set(:default_markup, :markdown) # set your favorite markup language
