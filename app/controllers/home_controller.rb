463edfd68f6b7f06c98d78415016e04f7ae007e7
kenharsch <kharsch@brandeis.edu>
2015-08-30 08:56:28 -0400

    added links to carousel

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,9 @@ class HomeController < ApplicationController
   def index
   	@posts = Post.all
   	@users = User.all
+  	@assignment = Assignment.where("due >= ?", Time.zone.now).sort_by{|d| d[:due]}.first
+  	@assignment_link = '/'+'assignments/'+ @assignment.id.to_s
+  	@trending = @posts.sort_by{|p| p[:updated_at]}.last
+  	@trending_link = '/'+'posts/'+ @trending.id.to_s
   end
 end
